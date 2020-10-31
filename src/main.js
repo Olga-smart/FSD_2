4cdd61b3322cf11c570969262b5eaf14b8d20583
olga-smart <olgat.smart@gmail.com>
2020-10-31 20:57:32 +0700

    Connect item-quantity-dropdown and Cleave.js plugins

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
-var jquery = require("jquery");
-window.$ = window.jQuery = jquery;
-require('air-datepicker')
-require('air-datepicker/dist/css/datepicker.min.css')
+import './connect-jquery.js';
+import jquery from "jquery";
+
+require('air-datepicker');
+require('air-datepicker/dist/css/datepicker.min.css');
+
+import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.js';
+import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css';
+require('item-quantity-dropdown/lib/item-quantity-dropdown.min.js');
+require('item-quantity-dropdown/lib/item-quantity-dropdown.min.css');
+
+$(document).ready(() => {
+  $('.iqdropdown').iqDropdown({});
+});
+
+import Cleave from 'cleave.js';
+
+var cleave = new Cleave('.birthday-input', {
+    date: true,
+    datePattern: ['d', 'm', 'Y']
+});
