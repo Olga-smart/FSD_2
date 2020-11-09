ce34e09d932a1f5b07c7a934c3a9e770e64e2f85
olga-smart <olgat.smart@gmail.com>
2020-11-09 12:01:27 +0700

    Configure masked text field

---
+++ src/components/input/input.js
@@ -0,0 +1,7 @@
+import Cleave from 'cleave.js';
+
+var cleave = new Cleave('.birthday-input input', {
+    date: true,
+    delimiter: '.',
+    datePattern: ['d', 'm', 'Y']
+});
--- src/main.js
+++ src/main.js
@@ -1,15 +1,7 @@
 import './connect-jquery.js';
 import jquery from "jquery";
 
-require('air-datepicker');
-require('air-datepicker/dist/css/datepicker.min.css');
-
-import Cleave from 'cleave.js';
-
-var cleave = new Cleave('.birthday-input', {
-    date: true,
-    datePattern: ['d', 'm', 'Y']
-});
-
 import './components/dropdown/dropdown.js';
+import './components/input/input.js';
+import './components/datepicker/datepicker.js';
 import './theme/style.scss';
