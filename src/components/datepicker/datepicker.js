174695811754d8b7b55ba9f3e0b5b85d7f7a0ff7
olga-smart <olgat.smart@gmail.com>
2020-11-10 23:49:26 +0700

    Override styles for datepicker

--- src/components/datepicker/datepicker.js
+++ src/components/datepicker/datepicker.js
@@ -4,3 +4,17 @@ require('air-datepicker/dist/css/datepicker.min.css');
 $('.datepicker-here').datepicker({
   clearButton: true
 })
+
+let datepickerBtns = document.querySelectorAll('.datepicker--button');
+for (let datepickerBtn of datepickerBtns) {
+  datepickerBtn.classList.add('button');
+}
+
+let datepickerApplyBtn = document.createElement('button');
+datepickerApplyBtn.textContent = 'Применить';
+datepickerApplyBtn.className = 'datepicker--button button';
+
+let datepickerBtnsContainers = document.querySelectorAll('.datepicker--buttons');
+for (let datepickerBtnsContainer of datepickerBtnsContainers) {
+  datepickerBtnsContainer.append(datepickerApplyBtn.cloneNode(true));
+}
--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,7 +1,7 @@
-import Cleave from 'cleave.js';
-
-var cleave = new Cleave('.birthday-input input', {
-    date: true,
-    delimiter: '.',
-    datePattern: ['d', 'm', 'Y']
-});
+//import Cleave from 'cleave.js';
+//
+//var cleave = new Cleave('.birthday-input input', {
+//    date: true,
+//    delimiter: '.',
+//    datePattern: ['d', 'm', 'Y']
+//});
