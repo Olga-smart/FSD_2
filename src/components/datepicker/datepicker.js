6b32c2005eed9a903c85b0dbe2e34284e67cee97
olga-smart <olgat.smart@gmail.com>
2020-11-11 23:49:33 +0700

    Configure logic for datepicker

--- src/components/datepicker/datepicker.js
+++ src/components/datepicker/datepicker.js
@@ -2,19 +2,58 @@ require('air-datepicker');
 require('air-datepicker/dist/css/datepicker.min.css');
 
 $('.datepicker-here').datepicker({
-  clearButton: true
+  clearButton: true,
+  navTitles: {
+    days: 'MM yyyy'
+  },
+  offset: 6
 })
 
-let datepickerBtns = document.querySelectorAll('.datepicker--button');
-for (let datepickerBtn of datepickerBtns) {
-  datepickerBtn.classList.add('button');
+let calendars = document.querySelectorAll('.datepicker');
+for (let calendar of calendars) {
+  addStyleForBtns(calendar);
+  addApplyBtn(calendar);
+  setWidthForCalendar(calendar);
 }
 
-let datepickerApplyBtn = document.createElement('button');
-datepickerApplyBtn.textContent = 'Применить';
-datepickerApplyBtn.className = 'datepicker--button button';
-
-let datepickerBtnsContainers = document.querySelectorAll('.datepicker--buttons');
-for (let datepickerBtnsContainer of datepickerBtnsContainers) {
-  datepickerBtnsContainer.append(datepickerApplyBtn.cloneNode(true));
+function addStyleForBtns(calendar) {
+  let btns = calendar.querySelectorAll('.datepicker--button');
+  for (let btn of btns) {
+    btn.classList.add('button');
+  }
 }
+
+function addApplyBtn(calendar) {
+  let applyBtn = document.createElement('button');
+  applyBtn.textContent = 'Применить';
+  applyBtn.className = 'datepicker--button button';
+  
+  let btnsContainer = calendar.querySelector('.datepicker--buttons');
+  btnsContainer.append(applyBtn);
+  addLogicForApplyBtn(applyBtn);
+}
+
+function addLogicForApplyBtn(btn) {
+  btn.addEventListener('click', function() {
+    let thisCalendar = btn.closest('.datepicker');
+    thisCalendar.classList.remove('active');
+  });
+}
+
+function setWidthForCalendar(calendar) {
+  let input = document.querySelector('.datepicker-here');
+  calendar.style.width = input.offsetWidth + 'px';
+  if (document.querySelector('.double-datepicker-wrap')) {
+    calendar.style.width = document.querySelector('.double-datepicker-wrap').offsetWidth + 'px';
+  }
+}
+
+$('#date-start').datepicker({ 
+  onSelect: function (fd, d, picker) { 
+    $("#date-start").val(fd.split("-")[0]);
+    $("#date-end").val(fd.split("-")[1]);
+  }
+});
+
+
+
