6edc4dcdb5347b18c56919e20acec52dd219b33e
olga-smart <olgat.smart@gmail.com>
2020-11-14 22:51:59 +0700

    Add rate component

--- src/main.js
+++ src/main.js
@@ -5,4 +5,5 @@ import './components/dropdown/dropdown.js';
 import './components/input/input.js';
 import './components/datepicker/datepicker.js';
 import './components/like/like.js';
+import './components/rate/rate.js';
 import './theme/style.scss';
