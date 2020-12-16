f32a7fa454ccb1e268ebcc0848834b6b3c738f46
olga-smart <olgat.smart@gmail.com>
2020-12-16 11:39:56 +0700

    Add header component

---
+++ src/components/header/header.js
@@ -0,0 +1,9 @@
+let headers = document.querySelectorAll('.header');
+for (let header of headers) {
+  let burger = header.querySelector('.js-header__burger');
+  let menu = header.querySelector('.js-header__menu')
+  burger.addEventListener('click', function() {
+    burger.classList.toggle('header__burger_menu_open');
+    menu.classList.toggle('header__menu_open');
+  });
+} 
--- src/main.js
+++ src/main.js
@@ -7,10 +7,11 @@ import './components/datepicker/datepicker.js';
 import './components/like/like.js';
 import './components/rate/rate.js';
 import './components/range/range.js';
-import './components/pagination/pagination.js';
+//import './components/pagination/pagination.js';
 import './components/expandable-checklist/expandable-checklist.js';
 import './components/comment/comment.js';
-import './components/card-booking/card-booking.js';
+//import './components/card-booking/card-booking.js';
 import './components/card-room/card-room.js';
+import './components/header/header.js';
 
 import './theme/style.scss';
