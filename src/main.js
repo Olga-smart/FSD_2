e61110164c9aff73d90e0d824b9cfddeef0173e4
olga-smart <olgat.smart@gmail.com>
2020-12-18 23:42:58 +0700

    Remove rempty rate.js

--- src/main.js
+++ src/main.js
@@ -5,12 +5,11 @@ import './components/dropdown/dropdown.js';
 import './components/input/input.js';
 import './components/datepicker/datepicker.js';
 import './components/like/like.js';
-import './components/rate/rate.js';
 import './components/range/range.js';
-//import './components/pagination/pagination.js';
+import './components/pagination/pagination.js';
 import './components/expandable-checklist/expandable-checklist.js';
 import './components/comment/comment.js';
-//import './components/card-booking/card-booking.js';
+import './components/card-booking/card-booking.js';
 import './components/card-room/card-room.js';
 import './components/header/header.js';
 
