f4f5ddcb4de1cbf036db325b5d6d6be7cbed6608
olga-smart <olgat.smart@gmail.com>
2020-12-18 23:10:16 +0700

    Refactor input component

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,7 +1,7 @@
-//import Cleave from 'cleave.js';
-//
-//var cleave = new Cleave('.birthday-input input', {
-//    date: true,
-//    delimiter: '.',
-//    datePattern: ['d', 'm', 'Y']
-//});
+import Cleave from 'cleave.js';
+
+var cleave = new Cleave('.js-input_birthday', {
+    date: true,
+    delimiter: '.',
+    datePattern: ['d', 'm', 'Y']
+});
