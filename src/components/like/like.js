52c3be41293acb4cd11ab3570aaeef0f74690aec
olga-smart <olgat.smart@gmail.com>
2020-12-18 23:39:40 +0700

    Refactor like component

--- src/components/like/like.js
+++ src/components/like/like.js
@@ -1,9 +1,9 @@
-let likes = document.querySelectorAll('.like');
+let likes = document.querySelectorAll('.js-like');
 for (let like of likes) {
   like.addEventListener('click', function() {
-    like.classList.toggle('checked');
+    like.classList.toggle('like_checked');
     let value = +like.textContent;
-    if ( like.classList.contains('checked') ) {
+    if ( like.classList.contains('like_checked') ) {
       like.textContent = ++value;
     } else {
       like.textContent = --value;
