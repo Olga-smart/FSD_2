029d151755fc26b1538e45480dc14e71967a880a
olga-smart <olgat.smart@gmail.com>
2021-01-26 11:47:45 +0700

    Improve and refactor input component + add comments to mixin

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,9 +1,11 @@
 import './input.scss';
 
-//import Cleave from 'cleave.js';
-//
-//var cleave = new Cleave('.js-input_birthday', {
-//    date: true,
-//    delimiter: '.',
-//    datePattern: ['d', 'm', 'Y']
-//});
+import Cleave from 'cleave.js';
+
+if (document.querySelector('.js-input_birthday')) {
+  let cleave = new Cleave('.js-input_birthday', {
+      date: true,
+      delimiter: '.',
+      datePattern: ['d', 'm', 'Y']
+  });
+}
