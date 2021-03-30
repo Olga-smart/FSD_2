abee7834891cd52e599bf98249500a98d859aa35
olga-smart <olgat.smart@gmail.com>
2021-03-30 23:19:32 +0700

    Rewrite datepicker component js code on classes

--- src/components/datepicker/datepicker.js
+++ src/components/datepicker/datepicker.js
@@ -1,87 +1,103 @@
 require('air-datepicker');
 require('air-datepicker/dist/css/datepicker.min.css');
 
-$('.datepicker-here').datepicker({
-  clearButton: true,
-  navTitles: {
-    days: 'MM yyyy'
-  },
-  offset: 6,
-  minDate: new Date(),
-  language: {
-    monthsShort: ['янв','фев','мар','апр','май','июн','июл','авг','сен','окт','ноя','дек']
-  },
-  range: true,
-  multipleDatesSeparator: ' - '
-})
+import './datepicker.scss';
+import '../input/input.js';
 
-let calendars = document.querySelectorAll('.datepicker');
-for (let calendar of calendars) {
-  addStyleForBtns(calendar);
-  addApplyBtn(calendar);
-}
-
-function addStyleForBtns(calendar) {
-  let btns = calendar.querySelectorAll('.datepicker--button');
-  for (let btn of btns) {
-    btn.classList.add('button');
-  }
-}
-
-function addApplyBtn(calendar) {
-  let applyBtn = document.createElement('button');
-  applyBtn.textContent = 'Применить';
-  applyBtn.className = 'datepicker--button button';
-  applyBtn.dataset.action = 'hide';
+class Datepicker {
   
-  let btnsContainer = calendar.querySelector('.datepicker--buttons');
-  btnsContainer.append(applyBtn);
-}
-
-let calendarInputs = document.querySelectorAll('.datepicker-here');
-for (let input of calendarInputs) {
-  let myDatepicker = $(input).datepicker().data('datepicker');
-  if (input.defaultValue) {    
-    if (input.defaultValue.includes(' - ')) {
-      myDatepicker.selectDate([
-        new Date( Date.parse( input.defaultValue.slice(0, 10) ) ),
-        new Date( Date.parse( input.defaultValue.slice(13) ) )
+  constructor(component) {
+    this._component = component;
+    
+    let options = {
+      clearButton: true,
+      navTitles: {
+        days: 'MM yyyy'
+      },
+      offset: 6,
+      minDate: new Date(),
+      language: {
+        monthsShort: ['янв','фев','мар','апр','май','июн','июл','авг','сен','окт','ноя','дек']
+      },
+      range: true,
+      multipleDatesSeparator: ' - '
+    }
+    
+    if (component.classList.contains('date-start')) {
+      options.onSelect = function(fd, d, picker) {
+        $(component).val(fd.split(" - ")[0]);
+        let dateEnd = component.parentElement.parentElement.nextElementSibling.querySelector('.date-end');
+        $(dateEnd).val(fd.split(" - ")[1]);
+      };
+    }
+    
+    if (component.classList.contains('date-end')) {
+      options.onSelect = function(fd, d, picker) {
+        let dateStart = component.parentElement.parentElement.previousElementSibling.querySelector('.date-start');
+        $(dateStart).val(fd.split(" - ")[0]);
+        $(component).val(fd.split(" - ")[1]);
+      };
+      options.position = "bottom right";
+    }
+    
+    $(component).datepicker(options);
+    
+    this._calendar = $(component).data('datepicker');
+    this._calendarElement = this._calendar.$datepicker[0];
+    
+    this._addApplyBtn();
+    this._addStyleForBtns();
+    
+    if (this._component.defaultValue) {
+      this._setDefaultDate()
+    }
+    
+    this._attachEventHandlers();  
+  } 
+  
+  _addApplyBtn() {
+    let applyBtn = document.createElement('button');
+    applyBtn.textContent = 'Применить';
+    applyBtn.className = 'datepicker--button button';
+    applyBtn.dataset.action = 'hide';
+    
+    let btnsContainer = this._calendarElement.querySelector('.datepicker--buttons');
+    btnsContainer.append(applyBtn);
+  }
+  
+  _addStyleForBtns() {
+    let btns = this._calendarElement.querySelectorAll('.datepicker--button');
+    for (let btn of btns) {
+      btn.classList.add('button');
+    }
+  }
+  
+  _setDefaultDate() {
+    if (this._component.defaultValue.includes(' - ')) {
+      this._calendar.selectDate([
+        new Date( Date.parse( this._component.defaultValue.slice(0, 10) ) ),
+        new Date( Date.parse( this._component.defaultValue.slice(13) ) )
       ]);
     } else {
-      myDatepicker.selectDate( new Date( Date.parse(input.defaultValue) ) );
+      this._calendar.selectDate( new Date( Date.parse(this._component.defaultValue) ) );
     }
   }
-  input.addEventListener('keydown', function(e) {
-    e.preventDefault();
-  });
+  
+  _attachEventHandlers() {
+    this._component.addEventListener('keydown', function(e) {
+      e.preventDefault();
+    });
+  }
+  
 }
 
-let dateStartCollection = document.querySelectorAll('.date-start');
-for (let dateStart of dateStartCollection) {
-  $(dateStart).datepicker({
-    onSelect: function(fd, d, picker) {
-      $(dateStart).val(fd.split(" - ")[0]);
-      let dateEnd = dateStart.parentElement.parentElement.nextElementSibling.querySelector('.date-end');
-      $(dateEnd).val(fd.split(" - ")[1]);
-    }
-  })
+let datepickers = document.querySelectorAll('.datepicker-here');
+for (let datepicker of datepickers) {
+  new Datepicker(datepicker);
 }
 
-let dateEndCollection = document.querySelectorAll('.date-end');
-for (let dateEnd of dateEndCollection) {
-  $(dateEnd).datepicker({
-    onSelect: function(fd, d, picker) {
-      let dateStart = dateEnd.parentElement.parentElement.previousElementSibling.querySelector('.date-start');
-      $(dateStart).val(fd.split(" - ")[0]);
-      $(dateEnd).val(fd.split(" - ")[1]);
-    },
-    position: "bottom right"
-  })
-}
 
-import './datepicker.scss';
 
-import '../input/input.js'
 
 
 
