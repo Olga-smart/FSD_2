492258045dabe1143534be055ec17a719ee4ccbf
olga-smart <olgat.smart@gmail.com>
2021-04-03 23:56:05 +0700

    Rewrite header component js code on classes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,12 +3,24 @@ import './header.scss';
 import '../logo/logo.js'
 import '../button/button.js'
 
+class Header {
+  
+  constructor(component) {
+    this._component = component;
+    this._burger = component.querySelector('.js-header__burger');
+    this._menu = component.querySelector('.js-header__menu');
+    this._attachEventHandlers();
+  }
+  
+  _attachEventHandlers() {
+    this._burger.addEventListener('click', (event) => {
+      this._burger.classList.toggle('header__burger_menu_open');
+      this._menu.classList.toggle('header__menu_open');
+    });
+  }
+}
+
 let headers = document.querySelectorAll('.header');
 for (let header of headers) {
-  let burger = header.querySelector('.js-header__burger');
-  let menu = header.querySelector('.js-header__menu')
-  burger.addEventListener('click', function() {
-    burger.classList.toggle('header__burger_menu_open');
-    menu.classList.toggle('header__menu_open');
-  });
+  new Header(header);
 } 
