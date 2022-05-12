369eac4b65bb066302cb435f80d1a1678e6652c1
olga-smart <olgat.smart@gmail.com>
2022-05-12 23:10:46 +0700

    Get rid of code duplication on the Room details page

--- src/pages/room-details/room-details.js
+++ src/pages/room-details/room-details.js
@@ -6,26 +6,8 @@ import '../../components/card-booking/card-booking';
 import '../../components/comment/comment';
 import '../../components/bullet-list/bullet-list';
 import '../../components/diagram/diagram';
+import wordToPlural from '../../helpers/wordToPlural/wordToPlural';
 import './room-details.scss';
-
-function lastDigit(num) {
-  return +num.toString().slice(-1);
-}
-
-function last2Digits(num) {
-  return +num.toString().slice(-2);
-}
-
-function wordToPlural(number, wordWith1, wordWith2, wordWith5) {
-  let result = wordWith5;
-  if ([1, 21, 31, 41, 51, 61, 71, 81, 91].includes(last2Digits(number))) {
-    result = wordWith1;
-  }
-  if ([2, 3, 4].includes(lastDigit(number)) && ![12, 13, 14].includes(last2Digits(number))) {
-    result = wordWith2;
-  }
-  return result;
-}
 
 const commentsNumber = document.querySelector('.js-room-details__comments-number');
 const commentsCollection = document.querySelectorAll('.js-room-details__comment');
