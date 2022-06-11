d5b6d54bf7eab39ceb43695ef56c6c00ca8864e3
Olga_Tarasova <olyaaasyaaa@gmail.com>
2022-06-12 00:19:12 +0700

    Get rid of extra global styles #7

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -4,8 +4,8 @@ import Cleave from 'cleave.js';
 
 import './input.scss';
 
-if (document.querySelector('.js-input_birthday')) {
-  new Cleave('.js-input_birthday', {
+if (document.querySelector('.js-input_date')) {
+  new Cleave('.js-input_date', {
     date: true,
     delimiter: '.',
     datePattern: ['d', 'm', 'Y'],
